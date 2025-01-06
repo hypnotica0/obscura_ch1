e766da2bb7bd57bd16200cb09eb4d5a7b10e7f74
hypnotica_ <killercrayon991@gmail.com>
2025-01-05 22:13:02 -0600

    new

---
+++ src/main/java/net/hypnotica/obscura_ch1/Cinderfall.java
@@ -0,0 +1,43 @@
+package net.hypnotica.obscura_ch1;
+
+import com.mojang.logging.LogUtils;
+import net.hypnotica.obscura_ch1.item.ModItems;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(Cinderfall.MOD_ID)
+public class Cinderfall {
+    public static final String MOD_ID = "obscura_ch1";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public Cinderfall() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        ModItems.register(modEventBus);
+        modEventBus.addListener(this::commonSetup);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+
+    }
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+
+        }
+    }
+}
